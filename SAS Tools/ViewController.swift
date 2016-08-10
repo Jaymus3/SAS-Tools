f92a672f9cfcef66af4ba0203e5b9d59a5a79f7a
James <zaleus3@gmail.com>
2016-08-10 10:40:08 -0500

    Initial Commit

---
+++ SAS Tools/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SAS Tools
+//
+//  Created by Sturtevant Auto on 8/10/16.
+//  Copyright © 2016 Sturtevant Auto. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
